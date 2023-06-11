5cbc1ec98ca55b38e32088229a981fbe7e89b719
Razeen <makerrazeen2301@gmail.com>
2023-06-11 09:49:20 +0530

    commit four, module 2 & 6 , error resolved
    The action is working fine, the error is resolved, The designing is halfway through and The webcam also working well, Moving on to recognizing the faces.

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
+
 import os
 import cv2 as cv
-
 
 cap = cv.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 
 # print(cap.isOpened())
-# img_background = cv.imread('Resources/background.png')
 img_background = cv.imread('Resources1/MacBook Air - 1.jpg')
 
-
-#import actions to list
+#import actions to list and resize it
 folder_action = 'Resources1/Actions'
-action_path = os.listdir(folder_action)#set the path to action_path by using operating system.list Directly
+action_path = os.listdir(folder_action)
 
 action_list = []
-# action_list[1] = 'Resources1/Actions/101.jpg'
-for path in action_path: #for loop to append the action to list
-    action_list.append(cv.imread(os.path.join(folder_action, path)))
+desired_width, desired_height = 405, 625
+#
+for path in action_path:
+    action_img = cv.imread(os.path.join(folder_action, path))
 
-# print(action_path[1])
-print(len(action_list))
-# print(action_path)
+# this line of code(next 3 line) is not necessary, i code it because i get some error  because of my macbook but
+    # for best we can use it , in macbook there is a file called .DS_Store ,it Automatically creates
+        # when we create a folder , it will Affect our program
+    if action_img is None:
+        print(f"failed to load image: {path}")
+        continue
 
+    # action_img = cv.resize(action_img, (405,625))
+    action_img = cv.resize(action_img, (desired_width, desired_height))
+
+    action_list.append(action_img)
+
+#  print(action_path[1])
+#  print(len(action_list))
+#  print(action_path)
 
 while True:
     success, webcam = cap.read()
 
     img_background[273:273 + 480, 103:103 + 640] = webcam
-    # img_background[180:180 + 610, 830:830 + 390] = action_list[1]
+#   img_background[173:173 + 610, 830:830 + 390] = action_list[0]
+    img_background[173:173 + desired_height, 823:823 + desired_width] = action_list[0]
 
     cv.imshow('face attendance', img_background)
-    # cv.imshow('web cam', webcam)
+#   cv.imshow('web cam', webcam)
+
     cv.waitKey(1)
+
+
+
+
+
+
+
