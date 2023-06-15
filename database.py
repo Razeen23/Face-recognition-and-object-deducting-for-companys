b20a0e008bd3959a15669b1bc0e3b4b6c07fc541
Razeen <makerrazeen2301@gmail.com>
2023-06-16 00:39:08 +0530

    commit six, module 8,9 completed
    The database has been created and the data are added to it, The real-time database is active now.

---
+++ database.py
@@ -0,0 +1,70 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# the key is more important because it's contains Confidential information
+cred = credentials.Certificate("service_account_key.json")
+
+# the {} for because it's a JSON file it have the key and values
+firebase_admin.initialize_app(cred, {'databaseURL': "https://company-attendance-database-default-rtdb.firebaseio.com/"})
+
+ref = db.reference('Employees')
+
+#create a JSON file inside a JSON file
+data = {
+    "123":
+        {
+            "name":"Elon",
+            "age":39,
+            "salary":500000.00,
+            "role":" manager",
+            "experience":6,
+            "total_attendance":80,
+            "last_attendance_time":"2023-06-15 00:00:00"
+
+        },
+    "456":
+        {
+            "name":"Mark",
+            "age":51,
+            "salary":600000.00,
+            "role":"full stack developer",
+            "experience":8,
+            "total_attendance":90,
+            "last_attendance_time":"2023-06-15 00:00:00"
+
+        },
+    "789":
+        {
+            "name":"Sundar",
+            "age":51,
+            "salary":400000.00,
+            "role":"tester",
+            "experience":12,
+            "total_attendance":85,
+            "last_attendance_time":"2023-06-15 00:00:00"
+
+        },
+    "2309":
+        {
+            "name":"Razeen",
+            "age":22,
+            "salary":15000.00,
+            "role":"python developer",
+            "experience":1,
+            "total_attendance":75,
+            "last_attendance_time":"2023-06-15 00:00:00"
+
+        }
+       }
+
+#in loop set the value to realtime database using the key
+for key,value in data.items():
+    ref.child(key).set(value)
+
+
+
+
+
+
+
